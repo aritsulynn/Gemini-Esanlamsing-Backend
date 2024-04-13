d31cfa98400dd2fd87f243df4a8dd60d7d995b28
Thanawat Jarusuthirug <lynniswaifu@gmail.com>
2024-04-13 13:48:29 +0700

    add mongodb

---
+++ src/main/java/com/example/demo/UserRepository.java
@@ -0,0 +1,6 @@
+package com.example.demo;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    User findByEmail(String email);
+}
